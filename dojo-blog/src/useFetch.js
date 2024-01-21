cb1f62054b0dd9c36f4778b2018705a85eecea8c
Josh O'Bryan <vespergo@gmail.com>
2024-01-21 17:36:18 -0600

    completed tutorial

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -1,28 +1,32 @@
 import { useState, useEffect } from "react";
-const useFetch = (url) =>{
-    const [data, setData] = useState(null);
-	const [isPending, setIsPending] = useState(true);    
-    const [error, setError] = useState(null);
+const useFetch = (url) => {
+	const [data, setData] = useState(null);
+	const [isPending, setIsPending] = useState(true);
+	const [error, setError] = useState(null);
 
-    useEffect(() => {
-		fetch(url)
+	useEffect(() => {
+		const abortCont = new AbortController();
+		fetch(url, { signal: abortCont.signal })
 			.then((res) => {
-                if(!res.ok){
-                    throw Error('could not fetch from server.');
-                }
+				if (!res.ok) {
+					throw Error("could not fetch from server.");
+				}
 				return res.json();
 			})
 			.then((data) => {
 				setData(data);
-                setError(null);
+				setError(null);
 				setIsPending(false);
 			})
 			.catch((e) => {
-				setError(e.message);
-                setIsPending(false);
+				if (e.name !== "AbortError") {
+					setError(e.message);
+					setIsPending(false);
+				}
 			});
+		return () => abortCont.abort();
 	}, [url]);
 
-    return {data, isPending, error}
-}
+	return { data, isPending, error };
+};
 export default useFetch;
